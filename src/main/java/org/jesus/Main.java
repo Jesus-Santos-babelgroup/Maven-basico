297a89b1e92669c4eb1aefc09232ea7838c26915
Jesús Santos <jesus.santos@babelgroup.com>
2025-03-10 10:48:35 +0100

    Basic exercise implemented

--- src/main/java/org/jesus/Main.java
+++ src/main/java/org/jesus/Main.java
@@ -1,17 +1,21 @@
 package org.jesus;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("");
+    public static void main(String[] args) throws JsonSyntaxException {
         Persona person = new Persona("Jesús", 23);
-        String personDTO = "persona: {name: Jesús, age: 23}";
+        Gson gson = new Gson();
+        String personDTO = gson.toJson(person);
         Persona p = jsonToPerson(personDTO);
         System.out.println(p);
     }
 
-    private static Persona jsonToPerson(String receivedJson) {
-        return receivedJson.fromJson();
+    private static Persona jsonToPerson(String receivedJson) throws JsonSyntaxException {
+        Gson gson = new Gson();
+        return gson.fromJson(receivedJson, Persona.class);
     }
 }
